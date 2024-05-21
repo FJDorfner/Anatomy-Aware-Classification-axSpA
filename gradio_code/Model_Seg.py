fixed rotation issue that occured for some images

gradio_code/Model_Seg.py
@@ -72,7 +72,6 @@ post_transforms = Compose([
 
 
 def load_and_segment_image(input_image_path, device):
-        
     image_tensor = pre_transforms(input_image_path)
     image_tensor = image_tensor.unsqueeze(0).to(device)
 
@@ -84,11 +83,7 @@ def load_and_segment_image(input_image_path, device):
 
     outputs = outputs.squeeze(0)
 
-    processed_outputs = post_transforms(outputs)
-
-    # rotate 
-    rotate = Rotate90(spatial_axes=(0, 1), k=3)
-    processed_outputs = rotate(processed_outputs).to('cpu')  
+    processed_outputs = post_transforms(outputs).to('cpu')  
 
     output_array = processed_outputs.squeeze().detach().numpy().astype(np.uint8)
 

gradio_code/app.py
@@ -5,8 +5,8 @@ import Model_Seg
 
 import SimpleITK as sitk
 import torch
-from numpy import uint8
-
+from numpy import uint8, rot90, fliplr
+from monai.transforms import Rotate90
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 image_base64 = utils.image_to_base64("anatomy_aware_pipeline.png")
@@ -68,14 +68,19 @@ def predict_image(input_image, input_file):
     image_mask = Model_Seg.load_and_segment_image(image_path, device)
 
     overlay_image_np, original_image_np = utils.overlay_mask(image_path, image_mask)
+    overlay_image_np = rot90(overlay_image_np, k=3)
+    overlay_image_np = fliplr(overlay_image_np)
 
     image_mask_im = sitk.GetImageFromArray(image_mask[None, :, :].astype(uint8))
     image_im = sitk.GetImageFromArray(original_image_np[None, :, :].astype(uint8))
     cropped_boxed_im, _ = utils.mask_and_crop(image_im, image_mask_im)
 
     cropped_boxed_array = sitk.GetArrayFromImage(cropped_boxed_im)
-    cropped_boxed_array_disp = cropped_boxed_array.squeeze()
     cropped_boxed_tensor = torch.Tensor(cropped_boxed_array)
+    rotate = Rotate90(spatial_axes=(0, 1), k=3)
+
+    cropped_boxed_tensor = rotate(cropped_boxed_tensor)
+    cropped_boxed_array_disp = cropped_boxed_tensor.numpy().squeeze().astype(uint8)
     prediction, image_transformed = Model_Class.load_and_classify_image(cropped_boxed_tensor, device)
 
 

gradio_code/utils.py
@@ -1,4 +1,4 @@
-from monai.transforms import Transform
+from monai.transforms import Transform, Compose, LoadImage, EnsureChannelFirst
 import torch
 import skimage
 import torch
@@ -13,6 +13,7 @@ import base64
 import numpy as np
 from cv2 import dilate
 from scipy.ndimage import label
+from Model_Seg import RgbaToGrayscale
 
 def image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
@@ -79,9 +80,14 @@ def custom_colormap():
     return cmap
 
 def read_image(image_path):
+    read_transforms = Compose([
+        LoadImage(image_only=True),
+        EnsureChannelFirst(),
+        RgbaToGrayscale(),  # Convert RGBA to grayscale
+    ])
     try:
-        original_image = Image.open(image_path).convert('L')
-        original_image_np = np.array(original_image)
+        original_image = read_transforms(image_path)
+        original_image_np = original_image.numpy().astype(np.uint8)
         return original_image_np.squeeze()
 
     except Exception as e: